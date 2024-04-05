6240244a92d2bbf515a858b70ac5febb8e7053d9
Gayathri11-2023 <150446843+Gayathri11-2023@users.noreply.github.com>
2024-04-05 11:13:42 +0530

    Update test_c2_3.py

--- test_c2_3.py
+++ test_c2_3.py
@@ -7,7 +7,6 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 import pytest
-from time import sleep
 
 
 # Defining a test class
@@ -24,32 +23,34 @@ class Test:
         # Setting up Chrome WebDriver with the WebDriver Manager
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         yield
+          # implementation of implicit wait
+       self.driver.implicitly_wait(10)
         self.driver.quit()
 
     @pytest.mark.html
     def test_login(self, boot):
-        # Opening the specified URL in the browser
-        self.driver.get(data.WebData().url)
-        # Maximizing the browser window
-        self.driver.maximize_window()
-        # Introducing a sleep delay for 7 seconds
-        sleep(7)
-        locator.WebLocators().entertext(self.driver, locator.WebLocators().usernameLocator, data.WebData().username)
-        locator.WebLocators().entertext(self.driver, locator.WebLocators().passwordLocator,
+        try:
+           # Opening the specified URL in the browser
+           self.driver.get(data.WebData().url)
+           # Maximizing the browser window
+           self.driver.maximize_window()
+           locator.WebLocators().entertext(self.driver, locator.WebLocators().usernameLocator, data.WebData().username)
+           locator.WebLocators().entertext(self.driver, locator.WebLocators().passwordLocator,
                                         data.WebData().password)
-        locator.WebLocators().clickbutton(self.driver, locator.WebLocators().buttonLocator)
-        sleep(2)
-        assert (self.driver.current_url == self.dashboard)
-        print("Successfully logged into the webpage of OrangeHRM")
-        locator.WebLocators().clickbutton(self.driver, locator.WebLocators().adminLocator)
-        sleep(3)
+           locator.WebLocators().clickbutton(self.driver, locator.WebLocators().buttonLocator)
+           assert (self.driver.current_url == self.dashboard)
+           print("Successfully logged into the webpage of OrangeHRM")
+           locator.WebLocators().clickbutton(self.driver, locator.WebLocators().adminLocator)
 
-        # Validate Menu options on the admin page
-        menu_options = ["Admin", "PIM", "Leave", "Time", "Recruitment",
+           # Validate Menu options on the admin page
+           menu_options = ["Admin", "PIM", "Leave", "Time", "Recruitment",
                         "My Info", "Performance", "Dashboard", "Directory", "Maintenance", "Buzz"]
-        for option in menu_options:
-            try:
-                self.driver.find_element_by_link_text(option)
-                print(f"{option} option is displayed.")
-            except:
+           for option in menu_options:
+              try:
+                 self.driver.find_element_by_link_text(option)
+                 print(f"{option} option is displayed.")
+              except:
                 print(f"{option} option is not displayed.")
+        
+        except NoSuchElementException as e:
+              Print("error")
