04ea9ede06915745de9a5a2ff9672dbbe0e6a082
Gayathri11-2023 <150446843+Gayathri11-2023@users.noreply.github.com>
2024-04-05 15:01:39 +0530

    Update test2.py

--- capstoneProject2/Testcases/test2.py
+++ capstoneProject2/Testcases/test2.py
@@ -6,54 +6,59 @@ from Locators import locator
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import NoSuchElementException
+# Explicit wait
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import pytest
-from time import sleep
 
 # Defining a test class
 
 
-class Test:
+class Test_case2:
     dashboard = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
 
     # pytest fixture for setting up the test environment
+    
     @pytest.fixture
     def boot(self, loginpage_title="OrangeHRM"):
         self.loginpage_title = loginpage_title
         # Setting up Chrome WebDriver with the WebDriver Manager
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         yield
+         # implementation of explicit wait
+        self.wait = WebDriverWait(self.driver, 10)
         self.driver.quit()
 
     @pytest.mark.html
-    def test_login(self, boot):
-        # Opening the specified URL in the browser
-        self.driver.get(data.WebData().url)
-        # Maximizing the browser window
-        self.driver.maximize_window()
-        # Introducing a sleep delay for 7 seconds
-        sleep(7)
-        locator.WebLocators().entertext(self.driver, locator.WebLocators().usernameLocator, data.WebData().username)
-        locator.WebLocators().entertext(self.driver, locator.WebLocators().passwordLocator,
-                                        data.WebData().password)
-        locator.WebLocators().clickbutton(self.driver, locator.WebLocators().buttonLocator)
-        sleep(2)
-        assert (self.driver.current_url == self.dashboard)
-        print("Successfully logged into the webpage of OrangeHRM")
-        locator.WebLocators().clickbutton(self.driver,locator.WebLocators().adminLocator)
-        sleep(3)
+    def test_header_validation(self, boot):
+        try:
+            # Opening the specified URL in the browser
+            self.driver.get(data.WebData().url)
+            # Maximizing the browser window
+            self.driver.maximize_window()
+            self.wait.until( EC.url_to_be(self.url) )
+            self.wait.until( EC.presence_of_element_located(locator.WebLocators().entertext(self.driver, locator.WebLocators().usernameLocator, data.WebData().username)))
+            self.wait.until( EC.presence_of_element_located(locator.WebLocators().entertext(self.driver, locator.WebLocators().passwordLocator,
+                                        data.WebData().password)))
+            self.wait.until( EC.presence_of_element_located(locator.WebLocators().clickbutton(self.driver, locator.WebLocators().buttonLocator)))
+            assert (self.driver.current_url == self.dashboard)
+            print("Successfully logged into the webpage of OrangeHRM")
+            self.wait.until( EC.presence_of_element_located(locator.WebLocators().clickbutton(self.driver,locator.WebLocators().adminLocator)))
+        
 
-        # Validate title of the admin page
-        assert (self.driver.title == self.loginpage_title)
-        print("Success: Web title verified")
+            # Validate title of the admin page
+            assert (self.driver.title == self.loginpage_title)
+            print("Success: Web title verified")
 
-        # Validate options on the admin page
-        admin_options = ["User Management", "Job", "Organization", "Qualifications", "Nationalities",
+            # Validate options on the admin page
+            admin_options = ["User Management", "Job", "Organization", "Qualifications", "Nationalities",
                          "Corporate Branding", "Configuration"]
-        for option in admin_options:
-            try:
-                self.driver.find_element_by_link_text(option)
-                print(f"{option} option is displayed.")
-            except:
-                print(f"{option} option is not displayed.")
-
-
+            for option in admin_options:
+                 try:
+                    self.driver.find_element_by_link_text(option)
+                    print(f"{option} option is displayed.")
+                 except:
+                    print(f"{option} option is not displayed.")
+      except NoSuchElementException as e:
+            print("error")
