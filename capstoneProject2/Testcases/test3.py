dabff0e470972ae550be860a4827bc3f3790c7cb
Gayathri11-2023 <150446843+Gayathri11-2023@users.noreply.github.com>
2024-04-05 15:09:14 +0530

    Update test3.py

--- capstoneProject2/Testcases/test3.py
+++ capstoneProject2/Testcases/test3.py
@@ -6,14 +6,17 @@ from Locators import locator
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import NoSuchElementException
+# Explicit wait
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import pytest
-from time import sleep
 
 
 # Defining a test class
 
 
-class Test:
+class Test_case3:
     dashboard = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
 
     # pytest fixture for setting up the test environment
@@ -24,32 +27,35 @@ class Test:
         # Setting up Chrome WebDriver with the WebDriver Manager
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         yield
+       # implementation of explicit wait
+        self.wait = WebDriverWait(self.driver, 10)
         self.driver.quit()
 
     @pytest.mark.html
-    def test_login(self, boot):
-        # Opening the specified URL in the browser
-        self.driver.get(data.WebData().url)
-        # Maximizing the browser window
-        self.driver.maximize_window()
-        # Introducing a sleep delay for 7 seconds
-        sleep(7)
-        locator.WebLocators().entertext(self.driver, locator.WebLocators().usernameLocator, data.WebData().username)
-        locator.WebLocators().entertext(self.driver, locator.WebLocators().passwordLocator,
-                                        data.WebData().password)
-        locator.WebLocators().clickbutton(self.driver, locator.WebLocators().buttonLocator)
-        sleep(2)
-        assert (self.driver.current_url == self.dashboard)
-        print("Successfully logged into the webpage of OrangeHRM")
-        locator.WebLocators().clickbutton(self.driver, locator.WebLocators().adminLocator)
-        sleep(3)
+    def test_mainmenu_validation(self, boot):
+        try:
+           # Opening the specified URL in the browser
+           self.driver.get(data.WebData().url)
+           # Maximizing the browser window
+           self.driver.maximize_window()
+           self.wait.until( EC.url_to_be(self.url) )
+           self.wait.until( EC.presence_of_element_located(locator.WebLocators().entertext(self.driver, locator.WebLocators().usernameLocator, data.WebData().username)))
+           self.wait.until( EC.presence_of_element_located(locator.WebLocators().entertext(self.driver, locator.WebLocators().passwordLocator,
+                                        data.WebData().password)))
+           self.wait.until( EC.presence_of_element_located(locator.WebLocators().clickbutton(self.driver, locator.WebLocators().buttonLocator)))
+           assert (self.driver.current_url == self.dashboard)
+           print("Successfully logged into the webpage of OrangeHRM")
+           self.wait.until( EC.presence_of_element_located(locator.WebLocators().clickbutton(self.driver, locator.WebLocators().adminLocator)))
 
-        # Validate Menu options on the admin page
-        menu_options = ["Admin", "PIM", "Leave", "Time", "Recruitment",
+           # Validate Menu options on the admin page
+           menu_options = ["Admin", "PIM", "Leave", "Time", "Recruitment",
                         "My Info", "Performance", "Dashboard", "Directory", "Maintenance", "Buzz"]
-        for option in menu_options:
-            try:
-                self.driver.find_element_by_link_text(option)
-                print(f"{option} option is displayed.")
-            except:
+           for option in menu_options:
+              try:
+                 self.driver.find_element_by_link_text(option)
+                 print(f"{option} option is displayed.")
+              except:
                 print(f"{option} option is not displayed.")
+        
+        except NoSuchElementException as e:
+              print("error")
