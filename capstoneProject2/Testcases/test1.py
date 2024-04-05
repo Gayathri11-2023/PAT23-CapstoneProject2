27aff33f0b6852f077937d45f1d975a115bf8566
Gayathri11-2023 <150446843+Gayathri11-2023@users.noreply.github.com>
2024-04-05 14:55:00 +0530

    Update test1.py

--- capstoneProject2/Testcases/test1.py
+++ capstoneProject2/Testcases/test1.py
@@ -6,12 +6,15 @@ from Locators import locator
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import NoSuchElementException
+# Explicit wait
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import pytest
-from time import sleep
 
 
 # Defining a test class
-class Test_case1:
+class Test:
     dashboard = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
 
     # pytest fixture for setting up the test environment
@@ -20,23 +23,22 @@ class Test_case1:
         # Setting up Chrome WebDriver with the WebDriver Manager
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         yield
+        # implementation of explicit wait
+        self.wait = WebDriverWait(self.driver, 10)
         self.driver.quit()
 
     @pytest.mark.html
-    def test_forgotpassword_link(self, boot):
-        # Opening the specified URL in the browser
-        self.driver.get(data.WebData().url)
-        # Maximizing the browser window
-        self.driver.maximize_window()
-        # Introducing a sleep delay for 7 seconds
-        sleep(7)
-        locator.WebLocators().entertext(self.driver, locator.WebLocators().usernameLocator, data.WebData().username)
-        sleep(2)
-        locator.WebLocators().clickbutton(self.driver, locator.WebLocators().forgotLocator)
-        sleep(2)
-        locator.WebLocators().x_path(self.driver, locator.WebLocators().username_Locator, data.WebData().username)
-        sleep(2)
-        locator.WebLocators().clickbutton(self.driver, locator.WebLocators().reset_Locator)
-        sleep(5)
-        print("successful : A reset password link has been sent to you via email.")
-
+    def test_login(self, boot):
+        try:
+           # Opening the specified URL in the browser
+            self.driver.get(data.WebData().url)
+           # Maximizing the browser window
+            self.driver.maximize_window()
+            self.wait.until( EC.url_to_be(data.WebData().url) )
+            self.wait.until( EC.presence_of_element_located(locator.WebLocators().entertext(self.driver, locator.WebLocators().usernameLocator, data.WebData().username)))
+            self.wait.until( EC.presence_of_element_located(locator.WebLocators().clickbutton(self.driver, locator.WebLocators().forgotLocator)))
+            self.wait.until( EC.presence_of_element_located(locator.WebLocators().x_path(self.driver, locator.WebLocators().username_Locator, data.WebData().username)))
+            self.wait.until( EC.presence_of_element_located(locator.WebLocators().clickbutton(self.driver, locator.WebLocators().reset_Locator)))
+            print("successful : A reset password link has been sent to you via email.")
+         except NoSuchElementException as e:
+             print("error")
